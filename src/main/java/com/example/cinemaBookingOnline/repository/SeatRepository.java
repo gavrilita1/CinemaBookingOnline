5d574543446d194c6c9044742c6101512bca7cb9
Rolland <gavrilita_rolland@yahoo.com>
2026-04-23 17:59:39 +0300

    security branch

--- src/main/java/com/example/cinemaBookingOnline/repository/SeatRepository.java
+++ src/main/java/com/example/cinemaBookingOnline/repository/SeatRepository.java
@@ -1,7 +1,6 @@
 package com.example.cinemaBookingOnline.repository;
 
 import com.example.cinemaBookingOnline.model.entities.Seat;
-import org.apache.logging.log4j.simple.internal.SimpleProvider;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
---
+++ src/main/java/com/example/cinemaBookingOnline/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.cinemaBookingOnline.security;
+
+import com.example.cinemaBookingOnline.model.entities.User;
+import com.example.cinemaBookingOnline.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole().name()))
+        );
+    }
+}
